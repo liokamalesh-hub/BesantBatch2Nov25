afa5353fc61812fa2c9ef4c86619bcb15b9d6d59
Dinesh107 <dineshjr107@gmail.com>
2025-12-17 19:25:13 +0530

    eigth commit

---
+++ index.js
@@ -0,0 +1,82 @@
+// Javascript - It is Web based programming language 
+
+// Most popular we broswers is made up of javascript
+
+document.getElementById("myH1").textContent = "Hello students";
+
+document.getElementById("myP").textContent = `welcome to javascript world`;
+
+
+// variable - a container that stores the value 
+
+// decalration  let a;
+
+// let var const 
+// assigment  a = 10;
+
+
+
+// Datatypes 
+
+// primitive and non primitive
+
+
+// primitive 
+
+// int  
+// Float
+// double
+// char 
+// boolean 
+
+// non primitive 
+
+// array 
+// string 
+// object 
+// collections(List, set, map, qoue)
+
+
+// let a;
+
+// a = 10;
+
+// let b;
+
+// b = 20;
+
+// let c = 10;
+
+// console.log(c);
+
+// let age = 24;
+// let price = 10.45;
+// let gpa = 2.4;
+// let isStudent = true;
+// let name = "siva"
+
+
+// console.log(typeof age);
+// console.log(typeof isStudent);
+// console.log(typeof name);
+
+// console.log(`You are ${age} years old`);
+// console.log(`The price is ${price}`);
+// console.log(`Your gpa is ${gpa}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
